866119d68115e489a3b85e36c13ec7906de6c38d
qazcde <qazcde.k@gmail.com>
2025-03-09 19:27:03 +0900

    4.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,29 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex items-center justify-center bg-slate-100 h-svh w-svw">
-      <div className="flex flex-col bg-white p-4 rounded-xl shadow-xl">
-        <div className="flex flex-row items-center justify-between">
-          <div className="flex flex-col justify-between">
-            <span className="text-lg text-gray-600 -mb-1">In transit</span>
-            <span className="text-3xl font-semibold">coolblue</span>
-          </div>
-          <div className="size-10 bg-amber-500 rounded-full" />
-        </div>
-        <div className="my-3">
-          <span className="bg-green-400 rounded-full px-4 py-1.5 mr-2 text-white font-semibold">
-            TODAY
-          </span>
-          <span>09:30-10:30u</span>
-        </div>
-        <div className="relative">
-          <div className="absolute w-full h-2 bg-gray-200 rounded-lg" />
-          <div className="absolute w-2/3 h-2 bg-green-200 rounded-lg" />
-          <div className="flex gap-4 mt-4">
-            <span>Expected</span>
-            <span>Sorting center</span>
-            <span>In transit</span>
-            <span className="text-gray-600">Delivered</span>
-          </div>
+    <div className="flex flex-col items-center justify-between min-h-screen p-6">
+      <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
+        <span className="text-9xl">🥕</span>
+        <h2 className="mt-4 text-3xl">당근 마겟에 어서오세요!</h2>
+      </div>
+      <div className="flex flex-col items-center gap-3 w-full">
+        <Link
+          href="/create-account"
+          className="w-full bg-orange-500 text-white text-lg font-semibold py-2.5 rounded-md text-center hover:bg-orange-400 transition-colors sm:w-96"
+        >
+          시작하기
+        </Link>
+        <div className="flex gap-2">
+          <span>이미 계정이 있나요?</span>
+          <Link
+            href="/login"
+            className="hover:underline text-customred font-semibold"
+          >
+            로그인
+          </Link>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
